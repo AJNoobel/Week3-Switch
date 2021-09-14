6b0b41d7a5b5c006642fd2f55b42dd1bb4664fdd
Opilane TTHK <opilane@tthk.ee>
2021-09-14 10:42:01 +0300

    Add Switch

---
+++ SwitchGradingProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwitchGradingProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ VowelsAndConsonants/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VowelsAndConsonants
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
